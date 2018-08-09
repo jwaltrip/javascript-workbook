419da6ca13c5297569411aae3ca7a12aee323dc2
jwaltrip <jake.waltrip@gmail.com>
2018-08-08 23:31:58 -0500

    initial mastermind commit

--- 04week/mastermind.js
+++ 04week/mastermind.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const colors = require('colors');
 const assert = require('assert');
 const readline = require('readline');
 const rl = readline.createInterface({
@@ -7,9 +8,14 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+// global vars
 let board = [];
 let solution = '';
 let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+
+function sanitizeInput(input) {
+  return input.toLowerCase().trim();
+}
 
 function printBoard() {
   for (let i = 0; i < board.length; i++) {
@@ -17,6 +23,9 @@ function printBoard() {
   }
 }
 
+// randomly generates 4 char solution
+// does not return anything
+// works on global vars
 function generateSolution() {
   for (let i = 0; i < 4; i++) {
     const randomIndex = getRandomInt(0, letters.length);
@@ -24,17 +33,51 @@ function generateSolution() {
   }
 }
 
+// generates/returns random int between input @min - @max
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function generateHint() {
-  // your code here
+function generateHint(guess) {
+  // create char array from solution and guess
+  const guessArr = guess.split('');
+  const solutionArr = solution.split('');
+
+  let correctLetterLocations = 0;
+
+  // iterate over solution, and find how many letters in guess are in correct location
+  solutionArr.forEach((letter, i) => {
+    if (letter === guessArr[i]) {
+      correctLetterLocations++;
+      letter = null;
+    }
+  });
+
+  let correctLetters = 0;
+
+  // iterate over solution, and find how many letters are correct but in the WRONG location
+  solutionArr.forEach((letter, i) => {
+    let targetIndex = guessArr.indexOf(i);
+
+    // if current letter in solutionsArr is found anywhere in the guessArr (if correct letter, but wrong location)
+    if (targetIndex > -1) {
+      correctLetters++;
+      letter = null;
+    }
+  });
+
+  const hintString = `${correctLetterLocations}`.red + `-${correctLetters}`;
+
+  return hintString;
+
 }
 
 function mastermind(guess) {
   solution = 'abcd'; // Comment this out to generate a random solution
-  // your code here
+
+  const guessCln = sanitizeInput(guess);
+
+
 }
 
 
